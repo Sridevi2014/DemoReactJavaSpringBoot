9ff3907448a1241e9149ad784556b5b06a7efaec
Sridevi Patchipulusu <sridevips@hotmail.com>
2020-05-17 21:08:31 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+## React and Java Spring Boot
+
+## React Front-End
+
+## Java Spring Boot Back-End
+
---
+++ src/test/java/com/ReactJavaSpringBoot/demoReactJavaSpringBoot/DemoReactJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ReactJavaSpringBoot.demoReactJavaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoReactJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
